e766d9de2e31e9851f7b01d64aa9ce71651cd19d
moshe <github email address>
2017-05-28 18:15:23 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 const app = "I don't do much.";
+const token = "02e80d42ed0ba472bd413961742c91748bfbfaba"
+function listRepos(){
+	fetch('https://api.github.com/user/repos',{
+		headers:{
+			Authorization: `token ${token}`
+		}
+	}).
+	then(res => res.json()).
+	then(json => console.log(json))
+}
